925caa484873b15cc55897769f23d34853b88b58
¨csportillo.ortiz@gmail.com¨ <csportillo.ortiz@gmail.com>
2018-07-14 15:23:18 -0600

    Add and Update modules and router

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { SharedModule } from "../shared/shared.module";
+import { PagesRoutingModule } from "./pages.routing";
+
+import { PagesComponent } from "./pages.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { ProgressComponent } from "./progress/progress.component";
+import { Graficas1Component } from "./graficas1/graficas1.component";
+
+@NgModule({
+  imports: [CommonModule, SharedModule, PagesRoutingModule],
+  declarations: [
+    PagesComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component
+  ],
+  exports: [
+    PagesComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component
+  ]
+})
+export class PagesModule {}
